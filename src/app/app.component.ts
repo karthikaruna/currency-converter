526ce7d57cf28f712d84dcbf6a6292107ca1a533
anusha288g <arumugam.karthikeyan7@gmail.com>
2022-12-24 10:26:45 +0530

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'currency-converter';
-}
+export class AppComponent {}
