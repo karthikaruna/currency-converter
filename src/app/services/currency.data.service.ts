00141713e1449e43dc6f98f703e1011919d7a0c9
anusha288g <arumugam.karthikeyan7@gmail.com>
2022-12-24 19:52:01 +0530

    Add #getSymbols

--- src/app/services/currency.data.service.ts
+++ src/app/services/currency.data.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+const API_BASE = 'https://api.exchangerate.host';
+
+interface Symbol {
+  code: string;
+  description: string;
+}
+
+interface SymbolsResponse {
+  symbols: {
+    [code: string]: Symbol
+  };
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyDataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getSymbols() {
+    return this.http.get<SymbolsResponse>(`${API_BASE}/symbols`).pipe(map((r) => Object.keys(r.symbols)));
+  }
 }
