96c6251931b87e0e4ce373c285b81e0143d2806e
anusha288g <arumugam.karthikeyan7@gmail.com>
2022-12-24 19:56:48 +0530

    Add conversion form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +24,14 @@ import { HistoryComponent } from './components/history/history.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CurrencyDataService } from 'src/app/services/currency.data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private currencyDataService: CurrencyDataService) {}
 
+  symbols: Array<string> = [];
+
+  ngOnInit() {
+    this.currencyDataService.getSymbols().subscribe((symbols) => this.symbols = symbols);
+  }
+
+  conversionForm = new FormGroup({
+    amount: new FormControl(null, Validators.required),
+    from: new FormControl(null, Validators.required),
+    to: new FormControl(null, Validators.required),
+  });
 }
